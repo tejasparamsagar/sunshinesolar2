509fe7f641ac5405aeda61bedebceab15705644d
tejasparamsagar <tejasparamsagar21@gmail.com>
2025-09-12 21:17:45 +0530

    added service.php and header.php

---
+++ header.php
@@ -0,0 +1,67 @@
+<div id="header-sticky" class="it-header-area it-header-ptb it-header-style black-bg">
+         <div class="container container-1650">
+            <div class="p-relative">
+               <div class="row align-items-center">
+                  <div class="col-xxl-2 col-xl-2 col-lg-6 col-md-6 col-6">
+                     <div class="it-header-logo">
+                        <a href="index-2.html"><img src="assets/img/logo/logo-white.png" alt=""></a>
+                     </div>
+                      <div class="it-header-logo-3 d-none">
+                        <a href="index-2.html"><img src="assets/img/logo/logo-black.png" alt=""></a>
+                     </div>
+                  </div>
+                  <div class="col-xxl-7 col-xl-7 d-none d-xl-block">
+                     <div class="it-header-menu it-header-dropdown">
+                        <nav class="it-menu-content">
+                           <ul>
+                              <li class=" p-static">
+                                 <a href="index.html">Home</a>
+                              </li>
+                              <li >
+                                 <a href="about-us-v1.html">About Us</a>
+                                 
+                              </li>
+                              <li>
+                                 <a href="service-v2.html">Services</a>
+                              </li>
+                              <li>
+                                 <a href="contact.html">Contact</a>
+                              </li>
+                           </ul>
+                        </nav>
+                     </div>
+                  </div>
+                  <div class="col-xxl-3 col-xl-3 col-lg-6 col-md-6 col-6">
+                     <div class="it-header-right-action d-flex justify-content-end align-items-center">
+                        <div class="it-header-user d-none d-xxl-block">
+                           <a href="sign-in.html">
+                              <svg width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                 <path d="M8 10C10.7614 10 13 7.76142 13 5C13 2.23858 10.7614 0 8 0C5.23858 0 3 2.23858 3 5C3 7.76142 5.23858 10 8 10Z" fill="currentcolor" />
+                                 <path d="M8 11.666C3.85977 11.6706 0.504609 15.0258 0.5 19.166C0.5 19.6262 0.873086 19.9993 1.33332 19.9993H14.6666C15.1269 19.9993 15.5 19.6262 15.5 19.166C15.4954 15.0258 12.1402 11.6706 8 11.666Z" fill="currentcolor" />
+                              </svg>
+                           </a>
+                        </div>
+                        <a href="contact.html" class="it-btn-theme d-none d-md-flex">
+                           <span>
+                              <span class="text-1">Get a Free Call</span>
+                              <span class="text-2">Get a Free Call</span>
+                           </span>
+                        </a>
+                        <div class="it-header-bar d-xl-none">
+                           <button class="it-menu-bar">
+                              <span>
+                                 <svg width="24" height="20" viewBox="0 0 24 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                       d="M10 18.3333C10 17.4128 10.7462 16.6667 11.6667 16.6667H21.6667C22.5872 16.6667 23.3333 17.4128 23.3333 18.3333C23.3333 19.2538 22.5872 20 21.6667 20H11.6667C10.7462 20 10 19.2538 10 18.3333ZM0 1.66667C0 0.746183 0.746183 0 1.66667 0H21.6667C22.5872 0 23.3333 0.746183 23.3333 1.66667C23.3333 2.58713 22.5872 3.33333 21.6667 3.33333H1.66667C0.746183 3.33333 0 2.58713 0 1.66667ZM0 10C0 9.07953 0.746183 8.33333 1.66667 8.33333H21.6667C22.5872 8.33333 23.3333 9.07953 23.3333 10C23.3333 10.9205 22.5872 11.6667 21.6667 11.6667H1.66667C0.746183 11.6667 0 10.9205 0 10Z"
+                                       fill="currentcolor" />
+                                 </svg>
+                              </span>
+                           </button>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
