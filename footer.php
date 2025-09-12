38d9ca180976dc61ccc35a4661d249698b5f6fd2
tejasparamsagar <tejasparamsagar21@gmail.com>
2025-09-12 22:37:55 +0530

    updates in services page

---
+++ footer.php
@@ -0,0 +1,144 @@
+<section class="it-footer-wrap it-footer-style-4 it-footer-style-2 z-index-1 black-bg pt-110 fix">
+         <img class="footer-bg" src="assets/img/shape/footer-5.jpg" alt="">
+         <div class="it-footer-area it-footer-border p-relative pt-100 pb-50">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 mb-50 wow itfadeUp" data-wow-duration=".9s"
+                     data-wow-delay=".3s">
+                     <div class="it-footer-widget it-footer-col-4-1">
+                        <div class="it-footer-widget-logo mb-20">
+                           <a href="index-2.html"><img src="assets/img/logo/logo-white3.png" alt=""></a>
+                        </div>
+                        <div class="it-footer-widget-text">
+                           <p>There are many variations of passages<br> of Lorem Ipsum available, but the majority<br>have suffered in some form, by injected</p>
+                        </div>
+                        <div class="it-footer-widget-btn">
+                           <a href="contact.html" class="it-btn-theme">
+                              <span>
+                                 <span class="text-1">Get A Quote</span>
+                                 <span class="text-2">Get A Quote</span>
+                              </span>
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-3 col-lg-2 col-md-6 col-sm-6 mb-50 wow itfadeUp" data-wow-duration=".9s"
+                     data-wow-delay=".5s">
+                     <div class="it-footer-widget it-footer-col-4-2">
+                        <h4 class="it-footer-widget-title">Company</h4>
+                        <div class="it-footer-widget-menu">
+                           <ul>
+                              <li><a href="#">Home</a></li>
+                              <li><a href="#">About Us</a></li>
+                              <li><a href="#">Services</a></li>
+                              <li><a href="#">Career</a></li>
+                              <li><a href="#">Contact us</a></li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 mb-50 wow itfadeUp" data-wow-duration=".9s"
+                     data-wow-delay=".7s">
+                     <div class="it-footer-widget it-footer-col-4-3">
+                        <h4 class="it-footer-widget-title">Services</h4>
+                        <div class="it-footer-widget-menu">
+                           <ul>
+                              <li><a href="#">Solar Planning</a></li>
+                              <li><a href="#">Solar Installation</a></li>
+                              <li><a href="#">Electricity storage</a></li>
+                              <li><a href="#">Solar Repairing</a></li>
+                              <li><a href="#">Bio gas Plant</a></li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 mb-50 wow itfadeUp" data-wow-duration=".9s"
+                     data-wow-delay=".9s">
+                     <div class="it-footer-widget it-footer-col-4-4 d-flex justify-content-xl-end">
+                        <div>
+                           <h4 class="it-footer-widget-title">Gallery</h4>
+                           <div class="it-footer-widget-gallery-wrap">
+                              <div class="it-footer-widget-gallery-item">
+                                 <a href="#">
+                                    <img src="assets/img/video/gallery-1.jpg" alt="">
+                                 </a>
+                              </div>
+                              <div class="it-footer-widget-gallery-item">
+                                 <a href="#">
+                                    <img src="assets/img/video/gallery-2.jpg" alt="">
+                                 </a>
+                              </div>
+                              <div class="it-footer-widget-gallery-item">
+                                 <a href="#">
+                                    <img src="assets/img/video/gallery-3.jpg" alt="">
+                                 </a>
+                              </div>
+                              <div class="it-footer-widget-gallery-item">
+                                 <a href="#">
+                                    <img src="assets/img/video/gallery-4.jpg" alt="">
+                                 </a>
+                              </div>
+                              <div class="it-footer-widget-gallery-item">
+                                 <a href="#">
+                                    <img src="assets/img/video/gallery-5.jpg" alt="">
+                                 </a>
+                              </div>
+                              <div class="it-footer-widget-gallery-item">
+                                 <a href="#">
+                                    <img src="assets/img/video/gallery-6.jpg" alt="">
+                                 </a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+         <!-- copyright-area-start -->
+         <div class="it-copyright-area">
+            <div class="container">
+               <div class="it-copyright-wrap">
+                  <div class="row align-items-center">
+                     <div class="col-lg-6 col-md-7 col-sm-6 wow itfadeUp" data-wow-duration=".9s" data-wow-delay=".5s">
+                       <div class="it-copyright-left text-center text-sm-start">
+                           <p class="mb-0">© 2025 <a href="index-2.html">Sunshine Solar</a> All rights reserved.</p>
+                        </div>
+                     </div>
+                     <div class="col-lg-6 col-md-5 col-sm-6 wow itfadeUp" data-wow-duration=".9s" data-wow-delay=".7s">
+                        <div class="it-copyright-social text-center text-sm-end">
+                           <a href="#">
+                              <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                 <path d="M9.96298 7.11365L16.0827 0H14.6325L9.31877 6.17668L5.07471 0H0.179688L6.59754 9.34024L0.179688 16.8H1.62994L7.24138 10.2772L11.7234 16.8H16.6184L9.96263 7.11365H9.96298ZM7.97666 9.42253L7.3264 8.49245L2.15249 1.09173H4.38L8.5554 7.06435L9.20566 7.99443L14.6332 15.7579H12.4057L7.97666 9.42289V9.42253Z" fill="currentcolor" />
+                              </svg>
+                           </a>   
+                           <a href="#">
+                              <svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M8.95285 9.36523L9.40508 6.4168H6.57617V4.50344C6.57617 3.6968 6.97132 2.91055 8.23841 2.91055H9.52461V0.400391C9.52461 0.400391 8.35733 0.201172 7.24136 0.201172C4.91146 0.201172 3.38867 1.61323 3.38867 4.16961V6.4168H0.798828V9.36523H3.38867V16.4929C3.90798 16.5744 4.44023 16.6168 4.98242 16.6168C5.52462 16.6168 6.05687 16.5744 6.57617 16.4929V9.36523H8.95285Z" fill="currentcolor" />
+                              </svg>
+                           </a>                     
+                           <a href="#">
+                           <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                 <path d="M13.6 1.20117H5.2C2.8804 1.20117 1 3.08158 1 5.40117V13.8012C1 16.1208 2.8804 18.0012 5.2 18.0012H13.6C15.9196 18.0012 17.8 16.1208 17.8 13.8012V5.40117C17.8 3.08158 15.9196 1.20117 13.6 1.20117Z" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                 <path d="M12.7559 9.07222C12.8596 9.77131 12.7402 10.4853 12.4147 11.1126C12.0892 11.7399 11.5742 12.2486 10.9429 12.5663C10.3116 12.8841 9.59622 12.9947 8.89847 12.8824C8.20071 12.7701 7.55612 12.4407 7.05639 11.941C6.55666 11.4412 6.22722 10.7966 6.11494 10.0989C6.00266 9.40113 6.11326 8.68574 6.431 8.05446C6.74874 7.42318 7.25744 6.90817 7.88475 6.58267C8.51206 6.25717 9.22604 6.13776 9.92513 6.24143C10.6382 6.34717 11.2984 6.67945 11.8081 7.1892C12.3179 7.69895 12.6502 8.35913 12.7559 9.07222Z" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                 <path d="M14.0176 4.98242H14.0287" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                              </svg>
+                           </a>
+                           <a href="#">
+                              <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M12.3087 5.68555C13.8162 5.68555 15.2618 6.28437 16.3277 7.35027C17.3936 8.41618 17.9925 9.86186 17.9925 11.3693V18.0003H14.2033V11.3693C14.2033 10.8668 14.0037 10.3849 13.6484 10.0296C13.2931 9.67431 12.8112 9.4747 12.3087 9.4747C11.8063 9.4747 11.3244 9.67431 10.9691 10.0296C10.6138 10.3849 10.4142 10.8668 10.4142 11.3693V18.0003H6.625V11.3693C6.625 9.86186 7.22382 8.41618 8.28973 7.35027C9.35563 6.28437 10.8013 5.68555 12.3087 5.68555Z" fill="currentcolor" />
+                              <path d="M3.78916 6.63281H0V18.0003H3.78916V6.63281Z" fill="currentcolor" />
+                              <path d="M1.89458 3.78916C2.94092 3.78916 3.78916 2.94092 3.78916 1.89458C3.78916 0.848231 2.94092 0 1.89458 0C0.848231 0 0 0.848231 0 1.89458C0 2.94092 0.848231 3.78916 1.89458 3.78916Z" fill="currentcolor" />
+                              </svg>
+                           </a>                    
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+         </div>
+         <!-- copyright-area-end -->
+
+      </section>
